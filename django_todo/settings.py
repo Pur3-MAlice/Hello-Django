63c7a19f67f9c4f86c91347fa0fd8d4d78d491f3
Pur3-MAlice <aliceford98@live.co.uk>
2023-08-10 10:14:01 +0000

    host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['alif-todo-app-7880c7e124a0.herokuapp.com']
 
 # Application definition
 
